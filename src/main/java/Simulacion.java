b9a2b2b0af24848fec26aa2689e69d6231443fab
morsamatias <matimorsa@est.frba.utn.edu.ar>
2018-05-10 05:50:46 -0300

    Final changes

--- src/main/java/Simulacion.java
+++ src/main/java/Simulacion.java
@@ -1,4 +1,6 @@
+import javax.xml.crypto.dom.DOMCryptoContext;
 import java.io.BufferedWriter;
+import java.io.CharArrayReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.Period;
@@ -14,20 +16,24 @@ public class Simulacion {
     /////////////GLOBAL VAR////////////////
 
     private static Integer N = 0;
+    private static Integer M = 0;
+    private static Integer I = 0;
     private static Integer HV = -1;
-    private static Integer T = 0;
+    private static Double T = 0.00;
     private static Integer TF;
-    private static Integer TPT = 0;
+    private static Double TPT = 0.00;
     private static Double[] NTLineMax;
     private static Double[] NTLineMin;
     private static Integer[] NTLine;
-    private static Integer[] TPS;
+    private static Double[] TPS;
     private static Integer[] lines;
-    private static Long[] PERCENTAGE;
-    private static Long[] STA;
-    private static Long[] STS;
-    private static Long[] STLL;
-    private static Long[] WAITINGTIME;
+    private static Double[] PERCENTAGE;
+    private static Double[] STA;
+    private static Double[] STS;
+    private static Double[] STLL;
+    private static Double[] WAITINGTIME;
+    private static BufferedWriter writer;
+    private static Integer NA = 864000/Integer.valueOf(String.valueOf(dailyArrival(random())).substring(0,6));
 
 
 
@@ -35,54 +41,65 @@ public class Simulacion {
 
 
     public static void main (String[] args) throws IOException {
-        System.out.println("Ingrese cantidad de colas: ");
-        Scanner inputColas = new Scanner( System.in );
 
-        N = Integer.valueOf(inputColas.nextLine());
+        writer = new BufferedWriter(new FileWriter("/home/matias/Escritorio/BitcoinLinesTest.xls"));
+        System.out.println("Ingrese cantidad de simulaciones que va a realizar: ");
+        Scanner amountOfSimulations = new Scanner(System.in);
+        M = Integer.valueOf(amountOfSimulations.nextLine());
 
-        TPS          = new Integer[N];
-        lines        = new Integer[N];
-        STA          = new Long[N];
-        STS          = new Long[N];
-        STLL         = new Long[N];
-        WAITINGTIME  = new Long[N];
-        PERCENTAGE   = new Long[N];
-        NTLine       = new Integer[N];
-        NTLineMin    = new Double[N];
-        NTLineMax    = new Double[N];
+        while (I <= M) {
+            T = 0.00;
+            System.out.println("Ingrese cantidad de colas: ");
+            Scanner inputLines = new Scanner(System.in);
 
-        initialize(TPS,-1);
-        initialize(lines,0);
-        initialize(NTLine,0);
-        initializeLong(PERCENTAGE, 0);
-        initializeLong(STA,0);
-        initializeLong(STS,0);
-        initializeLong(STLL,0);
-        initializeLong(WAITINGTIME,0);
-        initializeNTLineMax();
+            N = Integer.valueOf(inputLines.nextLine());
 
+            TPS = new Double[N];
+            lines = new Integer[N];
+            STA = new Double[N];
+            STS = new Double[N];
+            STLL = new Double[N];
+            WAITINGTIME = new Double[N];
+            PERCENTAGE = new Double[N];
+            NTLine = new Integer[N];
+            NTLineMin = new Double[N];
+            NTLineMax = new Double[N];
 
-
-        System.out.println("Ingrese tiempo final (en decimas de segundo): ");
-        Scanner inputTF = new Scanner( System.in );
-
-        TF = Integer.valueOf(inputTF.nextLine());
-
-
-        while(T<TF) {
-            simulation();
-        }
+            initializeDouble(TPS, -1.00);
+            initialize(lines, 0);
+            initialize(NTLine, 0);
+            initializeDouble(PERCENTAGE, 0.00);
+            initializeDouble(STA, 0.00);
+            initializeDouble(STS, 0.00);
+            initializeDouble(STLL, 0.00);
+            initializeDouble(WAITINGTIME, 0.00);
+            initializeNTLineMax();
 
 
-        for(int i = 0; i < lines.length; i++) {
-            if(!(lines[i] == 0)) {
-                TPT = HV;
-                empty();
+            System.out.println("Ingrese tiempo final (en decimas de segundo): ");
+            Scanner inputTF = new Scanner(System.in);
+
+            TF = Integer.valueOf(inputTF.nextLine());
+
+
+            while (T < TF) {
+                simulation();
             }
+
+
+            for (int i = 0; i < lines.length; i++) {
+                if (!(lines[i] == 0)) {
+                    TPT = -1.00;
+                    empty();
+                }
+            }
+
+            printAnswer();
+            I++;
         }
 
-        printAnswer();
 
+        writer.close();
 
     }
 
@@ -95,10 +112,10 @@ public class Simulacion {
 
 
     private static void simulation() {
-        Integer minTps = minTPS();
+        Double minTps = minTPS();
         Integer minTpsIndex = minTPSIndex(minTps);
 
-        if (((TPT >= minTps) && (TPT == HV)) && (minTps >= 0)) {
+        if (((TPT >= minTps) && (TPT == -1.00)) && (minTps >= 0)) {
             T = T + minTps;
             lines[minTpsIndex] = lines[minTpsIndex] - 1;
             processExit(minTpsIndex);
@@ -107,8 +124,9 @@ public class Simulacion {
 
         } else {
             T = TPT;
-            Integer NA = 864000/Integer.valueOf(String.valueOf(nextArrival(random())).substring(0,6));
-            TPT = T + NA;
+            //Integer NA = 864000/Integer.valueOf(String.valueOf(nextArrival(random())).substring(0,6));
+
+            TPT = T+NA ;
             //Integer linePosition = Integer.valueOf(String.valueOf(linesPosition()).substring(0, 1));
             Integer linePosition = linesPosition();
 
@@ -123,7 +141,7 @@ public class Simulacion {
             if (!(lines[linePosition] > 1)) {
                 if (previousEmpty) {
                     makeNextHV(linePosition);
-                    Integer TA = Integer.valueOf(String.valueOf(attentionTime()).substring(0,1));
+                    Double TA =  attentionTime(linePosition);
                     TPS[linePosition] = T + TA;
                     STA[linePosition] = STA[linePosition] + TA;
                     STLL[linePosition] = STLL[linePosition] + TPT;
@@ -140,7 +158,7 @@ public class Simulacion {
 
     private static void empty() {
 
-        Integer minTps = minTPS();
+        Double minTps = minTPS();
         //Integer minTpsIndex = minTPSIndex(minTps);
 
         T = T + minTps;
@@ -158,29 +176,35 @@ public class Simulacion {
 
     }
 
+    ////// ATTENTION TIME, WITH THIS METHOD THE LOWER NUMBER HAVE PRIORITY
 
-    private static double attentionTime() {
-
-        Integer rangeMin = 1;
-        Integer rangeMax = 10;
-
+    private static Double attentionTime(int line) {
+        Double rangeMax;
+        Double rangeMin = 0.00;
+        if (line != 0) {
+            rangeMax = 1200.00 - 600.00/line;
+        }else{
+            rangeMax = 150.00 ;
+        }
         return ThreadLocalRandom.current().nextDouble(rangeMin, rangeMax);
     }
 
     private static void processExit(Integer index) {
 
         if(lines[index] >= 1) {
-            Integer TA = Integer.valueOf(String.valueOf(attentionTime()).substring(0,1));
+            Double TA =attentionTime(index);
             TPS[index] = T + TA;
             STA[index] = STA[index] + TA;
 
         } else {
-            TPS[index] = HV;
-            for (int x = 0; x<lines.length; x++){
+            boolean flag = true;
+            TPS[index] = -1.00;
+            for (int x = 0; x<lines.length && flag; x++){
                 if (lines[x]>=0){
-                    Integer TA = Integer.valueOf(String.valueOf(attentionTime()).substring(0,1));
+                    Double TA = attentionTime(index);
                     TPS[x] = T + TA;
                     STA[x] = STA[x] + TA;
+                    flag = false;
 
                 }
             }
@@ -201,12 +225,12 @@ public class Simulacion {
         }
     }
 
-    private static double nextArrival(double r) {
+    private static double dailyArrival(double r) {
         double sigma = 106540;
         double mu = 128800;
 
         if(r > 0.9968724239) {
-            nextArrival(random());
+            dailyArrival(random());
         }
 
         return sigma * Math.sqrt(-2 * Math.log(1-r)) + mu;
@@ -238,28 +262,28 @@ public class Simulacion {
     */
 
     private static Integer linesPosition(){
-       Double probability =  ThreadLocalRandom.current().nextDouble(0, 5);
+       Double probability =  ThreadLocalRandom.current().nextDouble(0, 1);
 
 
-       return (lineDistribution(probability));
+       return (lineDistribution(bitcoinDistribution(probability*100)));
 
     }
 
 
-    private static Integer minTPS() {
+    private static Double minTPS() {
 
-        List<Integer> filterList = Arrays.asList(TPS).stream().filter(n -> n != -1).collect(Collectors.toList());
+        List<Double> filterList = Arrays.asList(TPS).stream().filter(n -> n != -1).collect(Collectors.toList());
         if(!(filterList).isEmpty()) {
             return Collections.min(filterList);
         }
-        return -1;
+        return -1.00;
     }
 
-    public static int minTPSIndex (int minimoValor) {
+    public static int minTPSIndex (Double minimoValor) {
 
         int index = -1;
 
-        List<Integer> filterList = Arrays.asList(TPS).stream().filter(n -> n != -1).collect(Collectors.toList());
+        List<Double> filterList = Arrays.asList(TPS).stream().filter(n -> n != -1).collect(Collectors.toList());
 
         if(!(filterList).isEmpty()) {
             for (int i = 0; (i < TPS.length) && (index == -1); i++) {
@@ -276,7 +300,6 @@ public class Simulacion {
 
     public static void printAnswer() throws IOException {
 
-        BufferedWriter writer = new BufferedWriter(new FileWriter("/home/matias/Escritorio/BitcoinLinesTest.xls"));
 
 
 
@@ -285,24 +308,28 @@ public class Simulacion {
         // Calculate the results, average of waiting team for each line
         for (int i = 0; i < lines.length; i++)
             if(NTLine[i]!=0) {
-                WAITINGTIME[i] = (STS[i] - STLL[i] - STA[i]) / NTLine[i];
+                WAITINGTIME[i] = (STS[i] - STLL[i] - STA[i])/ NTLine[i];
             }else{
-                WAITINGTIME[i] = Long.valueOf(0);
+                WAITINGTIME[i] = 0.00;
             }
 
         // Calculate the results, percentage of processed transactions in that line on total transactions.
         for (int i = 0; i < lines.length; i++) {
-            PERCENTAGE[i] = (Long.valueOf(NTLine[i] * 100)) / NTLineTotal;
+            if (NTLineTotal != 0){
+                PERCENTAGE[i] = (Double.valueOf(NTLine[i])*100) / NTLineTotal;
+                PERCENTAGE[i] = Math.floor(PERCENTAGE[i] * 100) / 100;
+            }else{
+                PERCENTAGE[i] = null;
+            }
         }
-        writer.write("i "+"\t"+"WAITINGTIME[i]"+"\t"+"PERCENTAGE[i]"+"\n");
+            writer.write("SIMULATION NUMBER" + I + "LINES: "+ N + "TIME: "+ TF );
+            writer.write("i "+"\t"+"WAITINGTIME[i]"+"\t"+"PERCENTAGE[i]"+"\n");
         for (int i = 0; i < lines.length; i++) {
             System.out.println("Waiting time in the line:" + (i + 1) + " = " + WAITINGTIME[i] + "\n" + "percentage of transactions in " +
-                    "the line " + (i + 1) + " of the total: " + PERCENTAGE[i]);
+                    "the line " + (i + 1) + " of the total: " +PERCENTAGE[i]);
             writer.write((i+1)+"\t"+String.valueOf(WAITINGTIME[i])+"\t"+PERCENTAGE[i]+"\n");
         }
 
-
-        writer.close();
     }
 
 
@@ -318,6 +345,13 @@ public class Simulacion {
         }
     }
 
+    public static void initializeDouble(Double[] lista, Double valorInicial){
+        for (int i = 0; i< lista .length; i++){
+            lista[i] = Double.valueOf(valorInicial);
+        }
+    }
+
+
     //IN BITS PER BYTE
 
     public static void initializeNTLineMax(){
@@ -326,7 +360,7 @@ public class Simulacion {
         Double x = 5.00 /N;
 
         for (int i = 0; i < NTLineMin.length; i++){
-            NTLineMax[i] = x*(i+1);
+            NTLineMax[i] = 5.00 - x*i;
         }
         initializeNTLineMin();
 
@@ -336,7 +370,7 @@ public class Simulacion {
         int i;
         Double x = 5.00 /N;
         for (i = 0; i < NTLineMin.length; i++) {
-            NTLineMin[i] = x * i;
+            NTLineMin[i] = 5.00 -  x * (i+1);
             System.out.println("NTLineMin = " + NTLineMin[i] + " NTLineMax[" + i + "] =  " + NTLineMax[i]);
         }
     }
@@ -344,27 +378,29 @@ public class Simulacion {
     public static Double bitcoinDistribution(Double random){
 
         if (random <= 49.01126709){
-            return ThreadLocalRandom.current().nextDouble(0.001, 0.01);
-        }else if(random <= 49.01126709 + 22.85238066){
-            return ThreadLocalRandom.current().nextDouble(0.01, 0.1);
-        }else if (random <=  49.01126709 + 22.85238066 + 17.23819381){
-            return ThreadLocalRandom.current().nextDouble(0.1, 1);
-        }else if (random <= 49.01126709 + 22.85238066 + 17.23819381 + 7.686242931 ){
+            return ThreadLocalRandom.current().nextDouble(0, 1);
+        }else if(random <= (49.01126709 + 22.85238066)){
+            return ThreadLocalRandom.current().nextDouble(0.5, 2);
+        }else if (random <=  (49.01126709 + 22.85238066 + 17.23819381)){
             return ThreadLocalRandom.current().nextDouble(1, 3);
-        }else if (random <= 49.01126709 + 22.85238066 + 17.23819381 + 7.686242931 + 2.535713917){
-            return ThreadLocalRandom.current().nextDouble(1, 5);
-        }else if(random <= 49.01126709 + 22.85238066 + 17.23819381 + 7.686242931 + 2.535713917+ 0.5973369618){
-            return ThreadLocalRandom.current().nextDouble(1, 5);
-        }else if(random <= 49.01126709 + 22.85238066 + 17.23819381 + 7.686242931 + 2.535713917+ 0.5973369618 + 0.07137256447){
+        }else if (random <= (49.01126709 + 22.85238066 + 17.23819381 + 7.686242931 )){
+            return ThreadLocalRandom.current().nextDouble(1.5, 5);
+        }else if (random <=( 49.01126709 + 22.85238066 + 17.23819381 + 7.686242931 + 2.535713917)){
             return ThreadLocalRandom.current().nextDouble(2, 5);
-        }else if(random <= 49.01126709 + 22.85238066 + 17.23819381 + 7.686242931 + 2.535713917+ 0.5973369618 + 0.07137256447+0.006991690987){
+        }else if(random <=( 49.01126709 + 22.85238066 + 17.23819381 + 7.686242931 + 2.535713917+ 0.5973369618)){
+            return ThreadLocalRandom.current().nextDouble(2.5, 5);
+        }else if(random <= ( 49.01126709 + 22.85238066 + 17.23819381 + 7.686242931 + 2.535713917+ 0.5973369618 + 0.07137256447)){
             return ThreadLocalRandom.current().nextDouble(3, 5);
-        }else if(random <= 49.01126709 + 22.85238066 + 17.23819381 + 7.686242931 + 2.535713917+ 0.5973369618 + 0.07137256447+0.006991690987+ 0.0004867345059) {
+        }else if(random <= (49.01126709 + 22.85238066 + 17.23819381 + 7.686242931 + 2.535713917+ 0.5973369618 + 0.07137256447+0.006991690987)){
+            return ThreadLocalRandom.current().nextDouble(3.5, 5);
+        }else if(random <= (49.01126709 + 22.85238066 + 17.23819381 + 7.686242931 + 2.535713917+ 0.5973369618 + 0.07137256447+0.006991690987+ 0.0004867345059)) {
             return ThreadLocalRandom.current().nextDouble(4, 5);
+        }else{
+            return ( ThreadLocalRandom.current().nextDouble(4.5, 5));
         }
 
-        return ( ThreadLocalRandom.current().nextDouble(4.5, 5));
     }
+
 
 
 
